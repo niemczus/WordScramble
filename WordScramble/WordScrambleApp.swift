b9c39665bfcfc664643c08dd2329ff31e7159287
niemczus <87914966+niemczus@users.noreply.github.com>
2022-01-07 12:00:58 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kamil on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
